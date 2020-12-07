ff7f1549875269bb04ef320f999d8a46ae1c8f30
cynthiateeters <glean@cynthiateeters.com>
2020-12-07 16:40:19 -0500

    finish code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const sum = (a, b) => a + b;
-module.exports = sum;
+function chunkyMonkey(values, size) {
+  const outArr = [];
+
+  for (i = 0; i < values.length; i += size) {
+    outArr.push(values.slice(i, i + size));
+  }
+  return outArr;
+}
+
+module.exports = chunkyMonkey;
--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,55 @@
-const sum = require('../src/index');
+const { expect } = require('@jest/globals');
+const chunkyMonkey = require('../src/index');
 
 /**
  * Test Suite
  */
-describe('sum()', () => {
-  it('returns value to 2 numbers added together', () => {
+describe('chunkyMonkey()', () => {
+  it('splits array into groups the length of size and returns as 2-d array', () => {
     // arrange
-    const a = 3;
-    const b = 10;
+    const values = ['a', 'b', 'c', 'd'];
+    const size = 2;
 
     // act
-    const result = sum(a, b);
+    const result = chunkyMonkey(values, size);
 
     // log
     console.log('result: ', result);
 
     // assert
-    expect(result).toBe(13);
+    expect(result).toEqual([
+      ['a', 'b'],
+      ['c', 'd'],
+    ]);
   });
 });
+
+describe('2nd Test chunkyMonkey()', () => {
+  it('splits array into groups the length of size and returns as 2-d array', () => {
+    // arrange
+    const values = [0, 1, 2, 3, 4, 5];
+    const size = 4;
+
+    // act
+    const result = chunkyMonkey(values, size);
+
+    // log
+    console.log('result: ', result);
+
+    // assert
+    expect(result).toEqual([
+      [0, 1, 2, 3],
+      [4, 5],
+    ]);
+  });
+});
+
+/* test('2nd splits array into groups the length of size and returns as 2-d array', () => {
+  expect(
+    chunkyMonkey([0, 1, 2, 3, 4, 5], 4).toBe([
+      [0, 1, 2, 3],
+      [4, 5],
+    ])
+  );
+});
+ */
