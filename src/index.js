de5a37a22682d2fb1bf26ba6ab27c8a717ab5e69
cynthiateeters <glean@cynthiateeters.com>
2020-12-09 13:55:19 -0500

    finish code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-const sum = (a, b) => a + b;
-module.exports = sum;
+function sumOddFibonacciNumbers(num) {
+  const fibs = [0, 1];
+  let newFib = 0;
+  let total = 1;
+
+  while (true) {
+    newFib = fibs[1] + fibs[0];
+    if (newFib > num) break;
+    fibs[0] = fibs[1];
+    fibs[1] = newFib;
+    if (newFib % 2) {
+      total += newFib;
+    }
+  }
+  return total;
+}
+
+module.exports = sumOddFibonacciNumbers;
--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,48 @@
-const sum = require('../src/index');
+const sumOddFibonacciNumbers = require('../src/index');
 
 /**
  * Test Suite
  */
-describe('sum()', () => {
-  it('returns value to 2 numbers added together', () => {
+describe('sumOddFibonacciNumbers()', () => {
+  it('returns sum of all odd Fibonnci numbers', () => {
     // arrange
-    const a = 3;
-    const b = 10;
+    const num = 10;
 
     // act
-    const result = sum(a, b);
+    const result = sumOddFibonacciNumbers(num);
 
     // log
-    console.log('result: ', result);
+    console.log('result 1: ', result);
 
     // assert
-    expect(result).toBe(13);
+    expect(result).toBe(10);
+  });
+
+  it('returns sum of all odd Fibonnci numbers 2nd example', () => {
+    // arrange
+    const num = 1000;
+
+    // act
+    const result = sumOddFibonacciNumbers(num);
+
+    // log
+    console.log('result 2: ', result);
+
+    // assert
+    expect(result).toBe(1785);
+  });
+
+  it('returns sum of all odd Fibonnci numbers 3rd example', () => {
+    // arrange
+    const num = 4000000;
+
+    // act
+    const result = sumOddFibonacciNumbers(num);
+
+    // log
+    console.log('result 3: ', result);
+
+    // assert
+    expect(result).toBe(4613732);
   });
 });
