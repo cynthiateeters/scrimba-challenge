052f969a2c357f3ab4fb472f3cd6de42665cb121
cynthiateeters <glean@cynthiateeters.com>
2020-12-11 13:46:46 -0500

    make solution simpler

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 function avoidObstacles(nums) {
-  const sortedNums = nums.sort((a, b) => {
-    return a - b;
-  });
   let step = 1;
   let test = true;
 
   while (test) {
     step += 1;
-    let stepNums = [];
-    nums.forEach((num, indx) => {
-      stepNums.push(indx * step);
-    });
     test = !nums.every((num) => {
-      return stepNums.every((step) => {
-        return num != step;
-      });
+      return num % step;
     });
+    if (step > 1000) break;
   }
   return step;
 }
