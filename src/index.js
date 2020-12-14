82fe12fc2e25a5c0e8bcdd5b2c825b1b38283fe1
cynthiateeters <glean@cynthiateeters.com>
2020-12-14 13:00:31 -0500

    finish code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const sum = (a, b) => a + b;
-module.exports = sum;
+function arrayMaximalAdjacentDifference(nums) {
+    //  write code here.
+    function reducer(accumulator, currentValue, currentIndex, array){
+      currentIndex -= 1;
+      if (currentIndex < 0 ) return accumulator;
+      let diff = Math.abs(currentValue - array[currentIndex])
+      return diff > accumulator ? diff : accumulator;
+    }
+    return nums.reduce(reducer, 0);
+}
+module.exports = arrayMaximalAdjacentDifference;
--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,34 @@
-const sum = require('../src/index');
+const arrayMaximalAdjacentDifference = require('../src/index');
 
 /**
- * Test Suite
- */
-describe('sum()', () => {
-  it('returns value to 2 numbers added together', () => {
-    // arrange
-    const a = 3;
-    const b = 10;
+* Test Suite 
+*/
+describe('arrayMaximalAdjacentDifference()', () => {
+    it('returns largest difference between adjacent values', () => {
+        // arrange
+        const nums = [2, 4, 1, 0];
+        
+        // act
+        const result = arrayMaximalAdjacentDifference(nums);
 
-    // act
-    const result = sum(a, b);
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(3);
+    });
 
-    // log
-    console.log('result: ', result);
+    it('returns largest difference between adjacent values example 2', () => {
+        // arrange
+        const nums = [2, 9, 1, 0];
+        
+        // act
+        const result = arrayMaximalAdjacentDifference(nums);
 
-    // assert
-    expect(result).toBe(13);
-  });
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(8);
+    });
 });
