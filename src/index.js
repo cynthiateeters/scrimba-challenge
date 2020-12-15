15a4b17530ac167e92307599c3af881044788e73
cynthiateeters <glean@cynthiateeters.com>
2020-12-15 14:56:58 -0500

    finish code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-const sum = (a, b) => a + b;
-module.exports = sum;
+const gallery = document.querySelector('.gallery');
+const previous = document.querySelector('.previous');
+const next = document.querySelector('.next');
+
+let position = 0;
+const dist = -220;
+const num = 4;
+const opacity = 0.3
+
+const change = (position) => {
+  gallery.style.transform = `translateX(${position * dist}px)`;
+}
+
+const doPrevious = previous.addEventListener('click', () => {
+  next.style.opacity = 1;
+  if ( position == 0) {
+    previous.style.opacity = opacity;
+    return
+  }
+  change(--position);
+});
+
+const doNext = next.addEventListener('click', () => {
+  previous.style.opacity = 1;
+  if ( position == num) {
+    next.style.opacity = opacity;
+    return;
+  }
+  change(++position);
+});
+
