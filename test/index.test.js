d511463c9714e659132c48dcd4c618a3ea06231d
cynthiateeters <glean@cynthiateeters.com>
2020-12-16 14:28:12 -0500

    add eslint, prettier, airbnb
    using https://github.com/karlhadwen/eslint-prettier-airbnb-react

--- test/index.test.js
+++ test/index.test.js
@@ -4,18 +4,18 @@ const sum = require('../src/index');
  * Test Suite
  */
 describe('sum()', () => {
-  it('returns value to 2 numbers added together', () => {
-    // arrange
-    const a = 3;
-    const b = 10;
+	it('returns value to 2 numbers added together', () => {
+		// arrange
+		const a = 3;
+		const b = 10;
 
-    // act
-    const result = sum(a, b);
+		// act
+		const result = sum(a, b);
 
-    // log
-    console.log('result: ', result);
+		// log
+		console.log('result: ', result);
 
-    // assert
-    expect(result).toBe(13);
-  });
+		// assert
+		expect(result).toBe(13);
+	});
 });
