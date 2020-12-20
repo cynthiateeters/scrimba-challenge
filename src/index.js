74666435f9912eb9d98826efe04ce1cda90e2b28
cynthiateeters <glean@cynthiateeters.com>
2020-12-20 17:33:22 -0500

    finish code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
+function domainType(domains) {
+  const domainNames = {
+    org: "organization",
+    com: "commercial",
+    net: "network",
+    info: "information",
+  };
+  return domains.map((domain) => {
+    const parts = domain.split(".");
+    return domainNames[parts[parts.length - 1]];
+  });
+}
 
-export { sum, subtract };
+export { domainType };
--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,25 @@
-import { sum, subtract } from "../src/index"; //using the ES6 modules
+import { domainType } from "../src/index"; //using the ES6 modules
 
 /**
  * Test Suite
  */
-describe("sum()", () => {
-  it("returns value to 2 numbers added together", () => {
+describe("domainType()", () => {
+  it("returns list of domain types", () => {
     // arrange
-    const a = 3;
-    const b = 10;
+    const domains = ["en.wiki.org", "codefights.com", "happy.net", "code.info"];
 
     // act
-    const result = sum(a, b);
+    const result = domainType(domains);
 
     // log
     console.log("result: ", result);
 
     // assert
-    expect(result).toBe(13);
-  });
-});
-describe("subtract()", () => {
-  it("returns value of one number subtracted from other", () => {
-    // arrange
-    const a = 13;
-    const b = 10;
-
-    // act
-    const result = subtract(a, b);
-
-    // log
-    console.log("result: ", result);
-
-    // assert
-    expect(result).toBe(3);
+    expect(result).toEqual([
+      "organization",
+      "commercial",
+      "network",
+      "information",
+    ]);
   });
 });
