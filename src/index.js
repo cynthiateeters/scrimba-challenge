9492085fdd070f75dcd23206c7ceec2c414f9dc8
cynthiateeters <glean@cynthiateeters.com>
2020-12-22 12:25:17 -0500

    simpler solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 function extractMatrixColumn(matrix, column) {
   //  write code here.
-  const arr = [];
-  for (let i = 0; i < matrix.length; i += 1) {
-    arr.push(matrix[i][column]);
-  }
-  return arr;
+  return matrix.map((row) => {
+    return row[column];
+  });
 }
 
 export { extractMatrixColumn };
